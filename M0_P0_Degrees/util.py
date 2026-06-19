958abe116cfdc25ea3c0df9df9e15203607e5e34
Vijay Menon <vijaymenon14@outlook.com>
2026-06-19 03:56:18 +0100

    Annotate classes and methods supplied in distribution code

--- M0_P0_Degrees/util.py
+++ M0_P0_Degrees/util.py
@@ -1,23 +1,29 @@
+# Defines node class and constructor methods for state/parent/action
 class Node():
     def __init__(self, state, parent, action):
         self.state = state
         self.parent = parent
         self.action = action
 
-
+# Defines a STACK frontier (last in,first out or depth-first)
 class StackFrontier():
+    # Constructor method defining the initial froniter as empty list
     def __init__(self):
         self.frontier = []
 
+    # Defines "add" method - simply appends node to frontier list
     def add(self, node):
         self.frontier.append(node)
 
+    # Checks if the current frontier contains nodes with the input state
     def contains_state(self, state):
         return any(node.state == state for node in self.frontier)
 
+    # Checks if the current frontier is empty (True means empty)
     def empty(self):
         return len(self.frontier) == 0
 
+    # Depth-first node removal from frontier
     def remove(self):
         if self.empty():
             raise Exception("empty frontier")
@@ -26,7 +32,9 @@ class StackFrontier():
             self.frontier = self.frontier[:-1]
             return node
 
-
+# Breadth-first node removal from frontier
+# This class definition of the Queue Frontier enables it to inherit all
+# other methods from Stack Frontier, changing only the remove method
 class QueueFrontier(StackFrontier):
 
     def remove(self):
