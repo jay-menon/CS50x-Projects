193e27feab396ac518de7d66c4ea49636538f739
Vijay Menon <vijaymenon14@outlook.com>
2026-07-10 20:44:42 +0100

    Write section of minimax function that maps out whole node treeand now need to write section that traces up from terminal nodes to find the best move

--- M0_P1_Tic_Tac_Toe/tictactoe.py
+++ M0_P1_Tic_Tac_Toe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from util import Node, StackFrontier, QueueFrontier
 
 X = "X"
 O = "O"
@@ -48,8 +49,9 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     curr_player = player(board)
-    board[action[0]][action[1]] = curr_player
-    return board
+    board_copy = [list(i) for i in list(board)]
+    board_copy[action[0]][action[1]] = curr_player
+    return board_copy
     raise NotImplementedError
 
 
@@ -106,9 +108,64 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    poss_actions = actions(board)
-    for thing in poss_actions:
-        return thing
+    curr_player = player(board)
+    action_util_dict = {key:[] for key in actions(board)}
+    #print(actions(board))
+    board_copy = list(board)
+    action_num = 0
+    for action in actions(board):
+        action_num += 1
+        #print(action_num)
+        # Initialise the search with initial node and frontier
+        initial_node = Node(result(board, action),board,action)
+        frontier = QueueFrontier()
+        frontier.add(initial_node)
+        # Iterate through all nodes until terminal condition met, store terminal nodes in set
+        # Also stores the utility of each node in node_dict
+        num = 0
+        while frontier.empty() is False:
+            curr_node = frontier.remove()
+            num += 1
+            if terminal(curr_node.state) is True:
+                action_util_dict[action].append(utility(curr_node.state))
+            else:
+                poss_actions = actions(curr_node.state)
+                poss_added = 0
+                for poss_action in poss_actions:
+                    frontier.add(Node(result(curr_node.state,poss_action), curr_node, poss_action))
+                    poss_added += 1
+                    #print(poss_added)
+        #print(num)
+    print(action_util_dict)
+    return (0,0)
+# Minimax node tree troubleshoot
+# - actions(board) does indded give all available actions
+# - on the first action iteration, the initial board is correct
+# - (NOW FIXED) it appears the frontier has not been clearing after each iteration on the for loop
+# - Terminal states outputted appear to be accurate
+# - Node tree part appears to have been fixed
+
+    if curr_player == "X":
+        action_min_util_dict = {action:min(action_util_dict[action]) for action in action_util_dict}
+        incumbent = -2
+        best_action = None
+        for action in action_min_util_dict:
+            if action_min_util_dict[action] > incumbent:
+                best_action = action
+                incumbent = action_min_util_dict[action]
+    else:
+        action_max_util_dict = {action:max(action_util_dict[action]) for action in action_util_dict}
+        incumbent = 2
+        best_action = None
+        for action in action_max_util_dict:
+            if action_max_util_dict[action] < incumbent:
+                best_action = action
+                incumbent = action_max_util_dict[action]       
+    return best_action
+
+
+
+
     raise NotImplementedError
 
 def count_XO(board):
@@ -124,3 +181,15 @@ def count_XO(board):
 # 2. .add() is method for SETS, not dictionaries
 # 3. {} creates an empty dictionary, NOT an empty set: set created by set()
 # 4. Sets and dictionaries are NOT indexable as they are UNORDERED collections (uses hashes)
+# When writing node tree
+# 1. When importing classes from diff file, need to use:
+#   from util import Node, StackFrontier, QueueFrontier
+
+# Learnt:
+# 1. The multiline comment below the function def gets attached to the function as its explanation
+# 2. list() only makes a copy of the OUTER list meaning for nested lists, the lists inside your shallow copy
+#       are still linked to the original copy and changing those also changes the original!!
+
+# Extras to include:
+# - Use symmetry to reduce early computatiom
+# - Alpha beta pruning
