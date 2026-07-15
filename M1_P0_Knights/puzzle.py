d2ec903bd1f69432b13d93f135c0e4f82fad1700
Vijay Menon <vijaymenon14@outlook.com>
2026-07-15 04:45:52 +0100

    Complete Puzzle 2

--- M1_P0_Knights/puzzle.py
+++ M1_P0_Knights/puzzle.py
@@ -37,7 +37,17 @@ knowledge1 = And(
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
+    Or(AKnight,AKnave),
+    Not(And(AKnight,AKnave)),
 
+    Or(BKnight,BKnave),
+    Not(And(BKnight,BKnave)),
+
+    Implication(AKnight, Or(And(AKnight,BKnight), And(AKnave,BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight,BKnight), And(AKnave,BKnave)))),
+
+    Implication(BKnight,  Or(And(AKnight,BKnave), And(AKnave,BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight,BKnave), And(AKnave,BKnight))))
 )
 
 # Puzzle 3
