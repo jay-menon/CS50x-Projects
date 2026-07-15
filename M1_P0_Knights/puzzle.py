ae75baa5c5bb159d4e8446661a06e5d1eee96140
Vijay Menon <vijaymenon14@outlook.com>
2026-07-15 04:45:24 +0100

    Complete Puzzle 1

--- M1_P0_Knights/puzzle.py
+++ M1_P0_Knights/puzzle.py
@@ -23,7 +23,14 @@ knowledge0 = And(
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
+    Or(AKnight,AKnave),
+    Not(And(AKnight,AKnave)),
 
+    Or(BKnight,BKnave),
+    Not(And(BKnight,BKnave)),
+
+    Implication(AKnight,And(AKnave,BKnave)),
+    Implication(AKnave, Not(And(AKnave,BKnave)))
 )
 
 # Puzzle 2
