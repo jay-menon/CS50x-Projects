76ae200bfb9cf9a6185d910f19cd7ba86cf34c19
Vijay Menon <vijaymenon14@outlook.com>
2026-08-13 20:44:05 +0100

    Write get_q_value() function

--- M4_P1_Nim/nim.py
+++ M4_P1_Nim/nim.py
@@ -101,7 +101,13 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+
+        # Access Q values from the Q value dictionary
+        key = (state, action)
+        if key in self.q:
+            return self.q[key]
+        else:
+            return 0
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
